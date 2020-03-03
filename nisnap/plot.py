e0c8b443fc81bc91c487e8531853c200c53d53bc
xgrg <>
2020-03-03 19:49:28 +0100

    ENH: added resource_name as option

--- nisnap/plot.py
+++ nisnap/plot.py
@@ -7,12 +7,10 @@ import tempfile
 import logging as log
 
 
-def snap_xnat(config_fp, experiment_id, fp, axes=('A', 'C', 'S'),
-    orig=True, opacity=10):
+def snap_xnat(config_fp, experiment_id, fp, resource_name='SPM12_SEGMENT_T2T1_COREG',
+    axes=('A', 'C', 'S'), orig=True, opacity=10):
 
-    resource_name = 'SPM12_SEGMENT_T2T1_COREG'
     wd = tempfile.gettempdir()
-
     # Downloading resources
     filepaths = ss.download_resources(config_fp, experiment_id, resource_name, wd)
 
@@ -36,6 +34,9 @@ def create_parser():
     parser.add_argument('--axes', required=False, default='ACS')
     parser.add_argument('--opacity', required=False, type=int)
     parser.add_argument('-e', '--experiment', required=False, type=str)
+    parser.add_argument('--rn', required=False, type=str,
+        default='SPM12_SEGMENT_T2T1_COREG2')
+
     parser.add_argument('-o', '--output', required=False, type=argparse.FileType('w'))
     parser.add_argument('--disable_warnings', required=False, action='store_true',
         default=False)
@@ -136,8 +137,8 @@ if __name__ == '__main__':
     axes = tuple([e for e in args.axes])
 
     if args.config:
-        snap_xnat(args.config.name, args.experiment, args.output.name, axes,
-            not args.nobg, args.opacity)
+        snap_xnat(args.config.name, args.experiment, args.output.name,
+            args.rn, axes, not args.nobg, args.opacity)
     else:
         snap_files([e.name for e in args.files], args.output.name,
             getattr(args.bg, 'name', None), axes, args.opacity)
