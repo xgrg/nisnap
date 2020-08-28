c1738678a0aa96a055fca9702efcbd96c83c2fb3
jhuguetn <jhuguetn@gmail.com>
2020-08-28 16:39:29 +0200

    FIX: Logging default level is WARNING, ignoring INFO events

--- nisnap/xnat.py
+++ nisnap/xnat.py
@@ -100,6 +100,7 @@ def __download_freesurfer6__(x, experiment_id, destination,
     aseg.__swap_fs__(bg, cache=cache)
 
     import logging as log
+    log.basicConfig(level=log.INFO)
     log.info('freesurfer_reg_to_native: %s' % freesurfer_reg_to_native)
 
     bg = filepaths[0]
