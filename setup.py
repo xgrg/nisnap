07c0f8f6d17305b18ab22e3d007eb5c5195d72a4
xgrg <goperto@gmail.com>
2020-10-09 15:06:56 +0200

    PEP8 style and fixed links to Github

--- setup.py
+++ setup.py
@@ -1,40 +1,41 @@
 import setuptools
 from setuptools import setup
-
+from nisnap import __version__
 import os.path as op
 this_directory = op.abspath(op.dirname(__file__))
 with open(op.join(this_directory, 'README.md')) as f:
     long_description = f.read()
-from nisnap import __version__
+
+
+download_url = 'https://github.com/xgrg/nisnap/-/archive/v0.3.7/'\
+    'nisnap-v0.3.7.tar.gz'
 
 
 setup(
-  name = 'nisnap',
-  install_requires=['coverage>=4.5',
-    'nose>=1.3',
-    'matplotlib>=3.1',
-    'pyxnat>=1.3',
-    'tqdm>=4.31',
-    'nibabel>=2.0',
-    'numpy>=1.16',
-    'urllib3>=1.24',
-    'ipython>=7.3',
-    'Pillow>=7.0'],
-  scripts=['bin/nisnap'],
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  version = __version__,
-  description = 'nisnap',
-  packages=setuptools.find_packages(),
-  author = 'Greg Operto',
-  author_email = 'goperto@barcelonabeta.org',
-  url = 'https://gitlab.com/xgrg/nisnap',
-  download_url = 'https://gitlab.com/xgrg/nisnap/-/archive/v0.3.3/nisnap-v0.3.3.tar.gz',
-  classifiers = ['Intended Audience :: Science/Research',
-      'Intended Audience :: Developers',
-      'Topic :: Scientific/Engineering',
-      'Operating System :: Unix',
-      'Programming Language :: Python :: 3.7' ],
-  package_data={'nisnap': ['utils/colormap.json']}
-
-)
+    name='nisnap',
+    install_requires=['coverage>=4.5',
+                      'nose>=1.3',
+                      'matplotlib>=3.1',
+                      'pyxnat>=1.3',
+                      'tqdm>=4.31',
+                      'nibabel>=2.0',
+                      'numpy>=1.16',
+                      'urllib3>=1.24',
+                      'ipython>=7.3',
+                      'Pillow>=7.0'],
+    scripts=['bin/nisnap'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=__version__,
+    description='nisnap',
+    packages=setuptools.find_packages(),
+    author='Greg Operto',
+    author_email='goperto@barcelonabeta.org',
+    url='https://github.com/xgrg/nisnap',
+    download_url=download_url,
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: Unix',
+                 'Programming Language :: Python :: 3.7'],
+    package_data={'nisnap': ['utils/colormap.json']})
